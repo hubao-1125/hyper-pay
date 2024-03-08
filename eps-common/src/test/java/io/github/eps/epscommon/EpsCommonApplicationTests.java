be75c59a57f23a7febd543837436f5b4cc5907ed
hubao <19473254+hubao-1125@users.noreply.github.com>
2024-03-08 10:34:30 +0800

    add common

---
+++ eps-common/src/test/java/io/github/eps/epscommon/EpsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eps.epscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
