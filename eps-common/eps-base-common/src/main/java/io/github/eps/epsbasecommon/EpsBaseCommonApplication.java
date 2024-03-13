f36efe81655e56a55741c2770bc3de47bbd1d6d5
hubao <19473254+hubao-1125@users.noreply.github.com>
2024-03-13 09:39:02 +0800

    add

---
+++ eps-common/eps-base-common/src/main/java/io/github/eps/epsbasecommon/EpsBaseCommonApplication.java
@@ -0,0 +1,13 @@
+package io.github.eps.epsbasecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsBaseCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpsBaseCommonApplication.class, args);
+    }
+
+}
---
+++ eps-common/eps-base-common/src/test/java/io/github/eps/epsbasecommon/EpsBaseCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eps.epsbasecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsBaseCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
