da139e43a4d8cdb59416bd5880474dbdb606fbde
hubao <19473254+hubao-1125@users.noreply.github.com>
2024-03-21 13:55:35 +0800

    add eps-dubbo-common

---
+++ eps-common/eps-dubbo-common/src/main/java/io/github/eps/eps/epsdubbocommon/EpsDubboCommonApplication.java
@@ -0,0 +1,13 @@
+package io.github.eps.eps.epsdubbocommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsDubboCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpsDubboCommonApplication.class, args);
+    }
+
+}
