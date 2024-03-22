1ac927eb0ae21995836481a13cbdc325619ff40c
hubao <19473254+hubao-1125@users.noreply.github.com>
2024-03-22 13:44:37 +0800

    add eps-sdk

---
+++ eps-sdk/src/main/java/io/github/eps/eps/epssdk/EpsSdkApplication.java
@@ -0,0 +1,13 @@
+package io.github.eps.eps.epssdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpsSdkApplication.class, args);
+    }
+
+}
---
+++ eps-sdk/src/test/java/io/github/eps/eps/epssdk/EpsSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eps.eps.epssdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
