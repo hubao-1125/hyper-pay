9a097ffd4a7a5af4c3d959c0e504a1db364f4219
hubao <19473254+hubao-1125@users.noreply.github.com>
2024-03-26 09:37:30 +0800

    add eps-service

---
+++ eps-service/src/main/java/io/github/eps/eps/epsservice/EpsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.eps.eps.epsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpsServiceApplication.class, args);
+    }
+
+}
---
+++ eps-service/src/test/java/io/github/eps/eps/epsservice/EpsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eps.eps.epsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
