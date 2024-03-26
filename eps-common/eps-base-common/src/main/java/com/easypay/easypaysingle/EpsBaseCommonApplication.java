339cd00caa848fa697ba79cb9d90e006509a0e40
hubao <douhuhu1125@gmail.com>
2024-03-26 20:44:15 +0800

    for push maven center repo test

--- eps-common/eps-base-common/src/main/java/com/easypay/easypaysingle/EpsBaseCommonApplication.java
+++ eps-common/eps-base-common/src/main/java/com/easypay/easypaysingle/EpsBaseCommonApplication.java
@@ -1,4 +1,4 @@
-package io.github.eps.epsbasecommon;
+package com.easypay.easypaysingle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ eps-common/eps-base-common/src/main/java/com/easypay/easypaysingle/test/Test.java
@@ -0,0 +1,10 @@
+package com.easypay.easypaysingle.test;
+
+import lombok.Data;
+
+@Data
+public class Test {
+
+    private String name;
+    private String address;
+}
