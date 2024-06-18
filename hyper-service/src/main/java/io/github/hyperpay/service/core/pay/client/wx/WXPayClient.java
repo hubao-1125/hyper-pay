ddeecc0d42515a9dec6d5cfc33419c2812c625ff
hubao-1125 <douhuhu1125@gmail.com>
2024-06-18 17:42:36 +0800

    abstraction wx v2 request&parse response method

--- hyper-service/src/main/java/io/github/hyperpay/service/core/pay/client/wx/WXPayClient.java
+++ hyper-service/src/main/java/io/github/hyperpay/service/core/pay/client/wx/WXPayClient.java
@@ -5,6 +5,7 @@ import com.ijpay.core.enums.SignType;
 import com.ijpay.core.kit.WxPayKit;
 import com.ijpay.wxpay.WxPayApi;
 import com.ijpay.wxpay.model.UnifiedOrderModel;
+import io.github.hyperpay.common.enums.PayTerminalEnum;
 import io.github.hyperpay.common.enums.PaywayEnum;
 import io.github.hyperpay.common.enums.ResponseErrorCodeEnum;
 import io.github.hyperpay.common.model.vo.request.pay.PayRequestVO;
@@ -67,37 +68,15 @@ public class WXPayClient extends BaseClient {
             put("wap_url", payConfigObj.getNotifyURL());
             put("wap_name", "pay_center");
         }};
-
-        UnifiedOrderModel build = UnifiedOrderModel.builder()
-                .appid(payConfigObj.getAppId())
-                .mch_id(payConfigObj.getMchId())
-                .nonce_str(WxPayKit.generateStr())
-                .sign_type(SignType.HMACSHA256.getType())
-                .body(payRequestVO.getGoodsBody())
-                .out_trade_no(payRequestVO.getMainOrderNumber())
-                .total_fee(MoneyUtil.castBigDecimalToPoint(payRequestVO.getAmount()).toString())
-                .spbill_create_ip(payRequestVO.getIp())
-                .notify_url(payConfigObj.getNotifyURL() + ConstantUtil.NOTIFY + ConstantUtil.LEFT_SLASH + ConstantUtil.PAY + PaywayEnum.WXPAY.getPayCode())
+        UnifiedOrderModel build = getUnifiedOrderBuilder(payRequestVO, payConfigObj)
                 .trade_type(WXConstantUtil.H5)
                 .scene_info(JSONUtil.createObj().set("h5_info", sceneMap).toString())
                 .build();
 
-        Map<String, String> paramMap = build.createSign(payConfigObj.getPartnerKey(), SignType.HMACSHA256);
-        log.info("WXPayClient h5 pay req paramMap:{}", paramMap);
-        String xmlResult = WxPayApi.pushOrder(false, paramMap);
-        log.info("WXPayClient h5 pay res xmlResult:{}", xmlResult);
-        WXPayConstantObj constantObj = JSONUtil.parseObj(WxPayKit.xmlToMap(xmlResult)).toBean(WXPayConstantObj.class);
-        if (Objects.isNull(constantObj)) {
-            return ResponseVO.fail(ResponseErrorCodeEnum.WX_RES_IS_NULL).build();
-        }
-
-        // return_code
-        if (!WxPayKit.codeIsOk(constantObj.getReturn_code())) {
-            return ResponseVO.fail().errorCode(constantObj.getReturn_code()).errorMsg(constantObj.getReturn_msg()).build();
-        }
-        // result_code
-        if (!WxPayKit.codeIsOk(constantObj.getResult_code())) {
-            return ResponseVO.fail().errorCode(constantObj.getErr_code()).errorMsg(constantObj.getErr_code_des()).build();
+        WXPayConstantObj constantObj = wxCommonRequest(payConfigObj, build, PayTerminalEnum.H5);
+        ResponseVO responseVO = wxCommonCodeCheck(constantObj);
+        if (!Objects.isNull(responseVO)) {
+            return responseVO;
         }
 
         return ResponseVO.success().wxPayResponseVO(
@@ -111,5 +90,67 @@ public class WXPayClient extends BaseClient {
                         )
                         .build()
         ).build();
+    }
+
+    private static UnifiedOrderModel.UnifiedOrderModelBuilder getUnifiedOrderBuilder(PayRequestVO payRequestVO,
+                                                                                     WXPayConfigObj payConfigObj) {
+        return UnifiedOrderModel.builder()
+                .appid(payConfigObj.getAppId())
+                .mch_id(payConfigObj.getMchId())
+                .nonce_str(WxPayKit.generateStr())
+                .sign_type(SignType.HMACSHA256.getType())
+                .body(payRequestVO.getGoodsBody())
+                .out_trade_no(payRequestVO.getMainOrderNumber())
+                .total_fee(MoneyUtil.castBigDecimalToPoint(payRequestVO.getAmount()).toString())
+                .spbill_create_ip(payRequestVO.getIp())
+                .notify_url(payConfigObj.getNotifyURL() + ConstantUtil.NOTIFY + ConstantUtil.LEFT_SLASH + ConstantUtil.PAY + PaywayEnum.WXPAY.getPayCode())
+                ;
+    }
+
+    private static ResponseVO wxCommonCodeCheck(WXPayConstantObj constantObj) {
+
+        if (Objects.isNull(constantObj)) {
+            return ResponseVO.fail(ResponseErrorCodeEnum.WX_RES_IS_NULL).build();
+        }
+        // return_code
+        if (!WxPayKit.codeIsOk(constantObj.getReturn_code())) {
+            return ResponseVO.fail().errorCode(constantObj.getReturn_code()).errorMsg(constantObj.getReturn_msg()).build();
+        }
+        // result_code
+        if (!WxPayKit.codeIsOk(constantObj.getResult_code())) {
+            return ResponseVO.fail().errorCode(constantObj.getErr_code()).errorMsg(constantObj.getErr_code_des()).build();
+        }
+
+        return null;
+    }
+
+    private static WXPayConstantObj wxCommonRequest(WXPayConfigObj payConfigObj, Object object, PayTerminalEnum payTerminalEnum) {
+
+        if (object instanceof UnifiedOrderModel) {
+            return wxCommonRequestUnifiedOrder(payConfigObj, (UnifiedOrderModel) object, payTerminalEnum);
+        }
+
+        return null;
+    }
+
+    private static WXPayConstantObj wxCommonRequestUnifiedOrder(WXPayConfigObj payConfigObj, UnifiedOrderModel build, PayTerminalEnum payTerminalEnum) {
+
+
+        switch (payTerminalEnum) {
+            case H5:
+                return wxCommonRequestUnifiedOrderH5(payConfigObj, build, payTerminalEnum);
+            default:
+                return null;
+        }
+
+    }
+
+    private static WXPayConstantObj wxCommonRequestUnifiedOrderH5(WXPayConfigObj payConfigObj, UnifiedOrderModel build, PayTerminalEnum payTerminalEnum) {
+
+        Map<String, String> paramMap = build.createSign(payConfigObj.getPartnerKey(), SignType.HMACSHA256);
+        log.info("WXPayClient h5 pay req paramMap:{}", paramMap);
+        String xmlResult = WxPayApi.pushOrder(false, paramMap);
+        log.info("WXPayClient h5 pay res xmlResult:{}", xmlResult);
+        return JSONUtil.parseObj(WxPayKit.xmlToMap(xmlResult)).toBean(WXPayConstantObj.class);
     }
 }
